aae72ae0eb20c2c0d293748512a92e6c60683f72
Steven West <stevenwest@Stevens-MacBook-Pro.local>
2019-10-08 09:51:38 -0500

    Fixed ability modifiers and calculating hit points correctly. Also prevented negative hit points

--- character.go
+++ character.go
@@ -32,7 +32,7 @@ const (
 	dbname = "postgres"
 )
 
-func calculateMaximumHealth(class string) int {
+func calculateMaximumHealth(class string, modifier int) int {
 	value := 0
 	switch class {
 	case "Barbarian":
@@ -60,6 +60,79 @@ func calculateMaximumHealth(class string) int {
 	case "Wizard":
 		value = Dice.RollDie(6)
 	}
+
+	if modifier <= 0 {
+		return value + 1
+	}
+	return value + modifier
+
+}
+
+func calculateAbilityModifiers(stat int) int {
+	value := 0
+	switch stat {
+	case 1:
+		value = -5
+	case 2:
+		value = -4
+	case 3:
+		value = -4
+	case 4:
+		value = -3
+	case 5:
+		value = -3
+	case 6:
+		value = -2
+	case 7:
+		value = -2
+	case 8:
+		value = -1
+	case 9:
+		value = -1
+	case 10:
+		value = 0
+	case 11:
+		value = 0
+	case 12:
+		value = 1
+	case 13:
+		value = 1
+	case 14:
+		value = 2
+	case 15:
+		value = 2
+	case 16:
+		value = 3
+	case 17:
+		value = 3
+	case 18:
+		value = 4
+	case 19:
+		value = 4
+	case 20:
+		value = 5
+	case 21:
+		value = 5
+	case 22:
+		value = 6
+	case 23:
+		value = 6
+	case 24:
+		value = 7
+	case 25:
+		value = 7
+	case 26:
+		value = 8
+	case 27:
+		value = 8
+	case 28:
+		value = 9
+	case 29:
+		value = 9
+	case 30:
+		value = 10
+
+	}
 	return value
 }
 
@@ -70,7 +143,7 @@ func generateCharacter(name string, class string) Character {
 	var strength = Dice.RollStat()
 	var dexterity = Dice.RollStat()
 	var constitution = Dice.RollStat()
-	var hitPointMaximum = calculateMaximumHealth(charClass) + constitution/2
+	var hitPointMaximum = calculateMaximumHealth(charClass, calculateAbilityModifiers(constitution))
 	var wisdom = Dice.RollStat()
 	var intelligence = Dice.RollStat()
 	var charisma = Dice.RollStat()
