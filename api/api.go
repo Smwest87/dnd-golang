2f3e35d11b3810bc98b28e319e655a8159639318
Smwest87 <steven@shipt.com>
2020-09-07 19:40:49 -0500

    Broke out into Config file and added TODO functions

--- api/api.go
+++ api/api.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/smwest87/dnd-golang/character"
+	config "github.com/smwest87/dnd-golang/configuration"
 )
 
 var password = os.Getenv("DB_PASSWORD")
@@ -21,26 +22,19 @@ func HomeLink(w http.ResponseWriter, r *http.Request) {
 func GetCharacter(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", character.Host, character.Port, character.User, password, character.Dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, password, config.Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal()
 	}
 
 	queryCharacter := "SELECT * FROM dnd.dnd_characters WHERE id = $1"
-
 	result := db.QueryRow(queryCharacter, key)
-
 	var returnCharacter character.Character
-
 	err = result.Scan(&returnCharacter.ID, &returnCharacter.Name, &returnCharacter.Class, &returnCharacter.Level, &returnCharacter.HitPointMaximum, &returnCharacter.Strength, &returnCharacter.Dexterity, &returnCharacter.Constitution, &returnCharacter.Wisdom, &returnCharacter.Intelligence, &returnCharacter.Charisma, &returnCharacter.Initiative, &returnCharacter.Modifiers)
-
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	fmt.Println(&returnCharacter.ID, &returnCharacter.Name, &returnCharacter.Level)
-
 	json.NewEncoder(w).Encode(returnCharacter)
 
 }
@@ -48,18 +42,24 @@ func GetCharacter(w http.ResponseWriter, r *http.Request) {
 func DeleteCharacter(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", character.Host, character.Port, character.User, password, character.Dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, password, config.Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal()
 	}
 
 	deleteCommand := "DELETE FROM dnd.dnd_characters WHERE id = $1"
-
 	result, err := db.Exec(deleteCommand, key)
 	if err != nil {
 		log.Fatal()
 	}
-
 	json.NewEncoder(w).Encode(result)
+}
+
+func CreateCharacter(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateCharacter(w http.ResponseWriter, r *http.Request) {
+
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,8 +12,10 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/home", api.HomeLink)
-	router.HandleFunc("/character/{id}", api.GetCharacter)
-	router.HandleFunc("/character/{id}", api.DeleteCharacter)
+	router.HandleFunc("/character/{id}", api.GetCharacter).Methods("GET")
+	router.HandleFunc("/character/{id}", api.DeleteCharacter).Methods("DELETE")
+	router.HandleFunc("/character/[{id}", api.UpdateCharacter).Methods("PUT")
+	router.HandleFunc("/character/new", api.CreateCharacter).Methods("POST")
 
 	/*hero, err := character.GenerateCharacter(os.Args[1], os.Args[2])
 	_, err = character.InsertCharacter(*hero)
@@ -20,6 +23,8 @@ func main() {
 		log.Fatal(err)
 	} */
 
-	http.ListenAndServe(":8080", router)
+	fmt.Println("Preparing to serve")
+	http.ListenAndServe(":10000", router)
+	fmt.Println("serve failed")
 
 }
