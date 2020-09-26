02fb6885cdab62bc9ae1631d9bc30fb3734e51fd
Smwest87 <steven@shipt.com>
2020-09-25 20:28:06 -0500

    Added return values for all API functions

--- api/api.go
+++ api/api.go
@@ -5,12 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"reflect"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
 	"github.com/smwest87/dnd-golang/character"
 	config "github.com/smwest87/dnd-golang/configuration"
 )
@@ -21,83 +21,93 @@ func HomeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-func GetCharacter(w http.ResponseWriter, r *http.Request) {
+func GetCharacter(w http.ResponseWriter, r *http.Request) (int, []byte, error) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, password, config.Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
 
 	queryCharacter := "SELECT * FROM dnd.dnd_characters WHERE id = $1"
 	result := db.QueryRow(queryCharacter, key)
 	var returnCharacter character.Character
-	err = result.Scan(&returnCharacter.ID, &returnCharacter.Name, &returnCharacter.Class, &returnCharacter.Level, &returnCharacter.HitPointMaximum, &returnCharacter.Strength, &returnCharacter.Dexterity, &returnCharacter.Constitution, &returnCharacter.Wisdom, &returnCharacter.Intelligence, &returnCharacter.Charisma, &returnCharacter.Initiative, &returnCharacter.Modifiers)
+	err = result.Scan(&returnCharacter.ID, &returnCharacter.Name, &returnCharacter.Class, &returnCharacter.Level, &returnCharacter.HitPointMaximum, &returnCharacter.Strength, &returnCharacter.Dexterity, &returnCharacter.Constitution, &returnCharacter.Wisdom, &returnCharacter.Intelligence, &returnCharacter.Charisma, &returnCharacter.Initiative, pq.Array(&returnCharacter.Modifiers))
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
 
-	json.NewEncoder(w).Encode(returnCharacter)
+	json_returnCharacter, err := json.Marshal(returnCharacter)
+
+	if err != nil {
+		return 400, nil, err
+	}
+
+	return 200, json_returnCharacter, nil
 
 }
 
-func DeleteCharacter(w http.ResponseWriter, r *http.Request) {
+func DeleteCharacter(w http.ResponseWriter, r *http.Request) (int, []byte, error) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, password, config.Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal()
+		return 400, nil, err
 	}
 
 	deleteCommand := "DELETE FROM dnd.dnd_characters WHERE id = $1"
 	_, err = db.Exec(deleteCommand, key)
 	if err != nil {
-		log.Fatal()
+		return 400, nil, err
 	}
+
+	return 200, nil, nil
 }
 
-func CreateCharacter(w http.ResponseWriter, r *http.Request) {
+func CreateCharacter(w http.ResponseWriter, r *http.Request) (int, []byte, error) {
 	returnCharacter := character.Character{}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
 	err = json.Unmarshal(body, &returnCharacter)
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
-	json.NewEncoder(w).Encode(returnCharacter)
 
 	hero, err := character.GenerateCharacter(returnCharacter.Name, returnCharacter.Class)
 
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
-		return
+		return 400, nil, err
 	}
 
 	_, err = character.InsertCharacter(*hero)
 
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
-		return
+		return 400, nil, err
 	}
 
-	json.NewEncoder(w).Encode(hero)
+	json_hero, err := json.Marshal(hero)
+	if err != nil {
+		return 400, nil, err
+	}
+
+	return 200, json_hero, nil
 
 }
 
-func UpdateCharacter(w http.ResponseWriter, r *http.Request) {
+func UpdateCharacter(w http.ResponseWriter, r *http.Request) (int, []byte, error) {
 	returnCharacter := character.Character{}
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
 	err = json.Unmarshal(body, &returnCharacter)
 	if err != nil {
-		json.NewEncoder(w).Encode(err)
+		return 400, nil, err
 	}
 
 	fields := reflect.TypeOf(returnCharacter)
